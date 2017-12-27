25a90b7d3e1dafb819387ed3bfc6d3840e99347b
1202kbs <1202kbs@gmail.com>
2017-12-27 18:54:03 +0900

    Add logit collection

--- models.py
+++ models.py
@@ -3,47 +3,35 @@ import tensorflow as tf
 
 class MNIST_DNN:
 
-    def __init__(self, sess, name):
-        self.sess = sess
+    def __init__(self, name):
         self.name = name
-        self._build_net()
 
-    def _build_net(self):
+    def __call__(self, X, training):
 
         with tf.variable_scope(self.name):
-            self.training = tf.placeholder(tf.bool)
-            self.X = tf.placeholder(tf.float32, [None, 784])
-            self.Y = tf.placeholder(tf.float32, [None, 10])
 
-            dense1 = tf.layers.dense(inputs=self.X, units=512, activation=tf.nn.relu)
-            dropout1 = tf.layers.dropout(inputs=dense1, rate=0.7, training=self.training)
+            with tf.variable_scope('layer1'):
+                dense1 = tf.layers.dense(inputs=X, units=512, activation=tf.nn.relu)
+                dropout1 = tf.layers.dropout(inputs=dense1, rate=0.7, training=training)
 
-            dense2 = tf.layers.dense(inputs=dropout1, units=512, activation=tf.nn.relu)
-            dropout2 = tf.layers.dropout(inputs=dense2, rate=0.7, training=self.training)
+            with tf.variable_scope('layer2'):
+                dense2 = tf.layers.dense(inputs=dropout1, units=512, activation=tf.nn.relu)
+                dropout2 = tf.layers.dropout(inputs=dense2, rate=0.7, training=training)
 
-            dense3 = tf.layers.dense(inputs=dropout2, units=512, activation=tf.nn.relu)
-            dropout3 = tf.layers.dropout(inputs=dense3, rate=0.7, training=self.training)
+            with tf.variable_scope('layer3'):
+                dense3 = tf.layers.dense(inputs=dropout2, units=512, activation=tf.nn.relu)
+                dropout3 = tf.layers.dropout(inputs=dense3, rate=0.7, training=training)
 
-            dense4 = tf.layers.dense(inputs=dropout3, units=512, activation=tf.nn.relu)
-            dropout4 = tf.layers.dropout(inputs=dense4, rate=0.7, training=self.training)
+            with tf.variable_scope('layer4'):
+                dense4 = tf.layers.dense(inputs=dropout3, units=512, activation=tf.nn.relu)
+                dropout4 = tf.layers.dropout(inputs=dense4, rate=0.7, training=training)
 
-            self.logits = tf.layers.dense(inputs=dropout4, units=10)
+            with tf.variable_scope('layer5'):
+                logits = tf.layers.dense(inputs=dropout4, units=10)
 
-        # define cost/loss & optimizer
-        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y))
-        self.optimizer = tf.train.AdamOptimizer().minimize(self.cost)
+        tf.add_to_collection('logits', logits)
 
-        correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    def predict(self, x_test, training=False):
-        return self.sess.run(self.logits, feed_dict={self.X: x_test, self.training: training})
-
-    def get_accuracy(self, x_test, y_test, training=False):
-        return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.training: training})
-
-    def train(self, x_data, y_data, training=True):
-        return self.sess.run([self.cost, self.optimizer], feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
+        return logits
 
     @property
     def vars(self):
@@ -52,57 +40,44 @@ class MNIST_DNN:
 
 class MNIST_CNN:
 
-    def __init__(self, sess, name):
-        self.sess = sess
+    def __init__(self, name):
         self.name = name
-        self._build_net()
 
-    def _build_net(self):
+    def __call__(self, X, training):
 
         with tf.variable_scope(self.name):
-            self.training = tf.placeholder(tf.bool)
-            self.X = tf.placeholder(tf.float32, [None, 784])
-            self.Y = tf.placeholder(tf.float32, [None, 10])
-            X_img = tf.reshape(self.X, [-1, 28, 28, 1])
 
             # Convolutional Layer #1 and Pooling Layer #1
-            conv1 = tf.layers.conv2d(inputs=X_img, filters=32, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
-            pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], padding="SAME", strides=2)
-            dropout1 = tf.layers.dropout(inputs=pool1, rate=0.7, training=self.training)
+            with tf.variable_scope('layer1'):
+                conv1 = tf.layers.conv2d(inputs=X, filters=32, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
+                pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], padding="SAME", strides=2)
+                dropout1 = tf.layers.dropout(inputs=pool1, rate=0.7, training=training)
 
             # Convolutional Layer #2 and Pooling Layer #2
-            conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
-            pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], padding="SAME", strides=2)
-            dropout2 = tf.layers.dropout(inputs=pool2, rate=0.7, training=self.training)
+            with tf.variable_scope('layer2'):
+                conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
+                pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], padding="SAME", strides=2)
+                dropout2 = tf.layers.dropout(inputs=pool2, rate=0.7, training=training)
 
-            # Convolutional Layer #2 and Pooling Layer #2
-            conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu)
-            pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], padding="same", strides=2)
-            dropout3 = tf.layers.dropout(inputs=pool3, rate=0.7, training=self.training)
+            # Convolutional Layer #2=3 and Pooling Layer #3
+            with tf.variable_scope('layer3'):
+                conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu)
+                pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], padding="same", strides=2)
+                dropout3 = tf.layers.dropout(inputs=pool3, rate=0.7, training=training)
 
             # Dense Layer with Relu
-            flat = tf.reshape(dropout3, [-1, 128 * 4 * 4])
-            dense4 = tf.layers.dense(inputs=flat, units=625, activation=tf.nn.relu)
-            dropout4 = tf.layers.dropout(inputs=dense4, rate=0.5, training=self.training)
+            with tf.variable_scope('layer4'):
+                flat = tf.reshape(dropout3, [-1, 128 * 4 * 4])
+                dense4 = tf.layers.dense(inputs=flat, units=625, activation=tf.nn.relu)
+                dropout4 = tf.layers.dropout(inputs=dense4, rate=0.5, training=training)
 
             # Logits (no activation) Layer: L5 Final FC 625 inputs -> 10 outputs
-            self.logits = tf.layers.dense(inputs=dropout4, units=10)
+            with tf.variable_scope('layer5'):
+                logits = tf.layers.dense(inputs=dropout4, units=10)
 
-        # define cost/loss & optimizer
-        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y))
-        self.optimizer = tf.train.AdamOptimizer().minimize(self.cost)
+            tf.add_to_collection('logits', logits)
 
-        correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    def predict(self, x_test, training=False):
-        return self.sess.run(self.logits, feed_dict={self.X: x_test, self.training: training})
-
-    def get_accuracy(self, x_test, y_test, training=False):
-        return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.training: training})
-
-    def train(self, x_data, y_data, training=True):
-        return self.sess.run([self.cost, self.optimizer], feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
+        return logits
 
     @property
     def vars(self):
