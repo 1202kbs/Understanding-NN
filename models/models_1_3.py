2dace5451c6102df51142a195e3704723094933e
1202kbs <1202kbs@gmail.com>
2018-01-01 21:28:56 +0900

    Fix typos

--- models/models_1_3.py
+++ models/models_1_3.py
@@ -7,7 +7,7 @@ class MNIST_DNN:
     def __init__(self, name):
         self.name = name
 
-    def __call__(self, X, training, reuse=False):
+    def __call__(self, X, reuse=False):
 
         with tf.variable_scope(self.name) as scope:
 
