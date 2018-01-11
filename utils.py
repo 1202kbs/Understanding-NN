e828888d0b5a2e4ff12a667cc9ac5f1e6de6641a
1202kbs <1202kbs@gmail.com>
2018-01-11 17:17:55 +0900

    add translate to utils

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+
 
 def plot(samples, X_dim, channel):
 	fig = plt.figure(figsize=(4, 4))
@@ -24,3 +26,8 @@ def plot(samples, X_dim, channel):
 			plt.imshow(sample.reshape([dim1, dim1, channel]))
 
 	return fig
+
+
+def translate(img, x, y):
+    M = np.float32([[1, 0, x], [0, 1, y]])
+    return cv2.warpAffine(img.reshape(28,28), M, (28, 28)).reshape(1,784)
